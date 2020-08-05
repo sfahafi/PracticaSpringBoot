423706122bf5b99a00d5793cd2b418392b473769
toro <toro@DESKTOP-990KAMI>
2020-08-05 20:44:44 -0300

    Validaciones springboot

--- HolaMundoThymeleaf/src/main/java/sf/com/spb/domain/Persona.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/domain/Persona.java
@@ -2,6 +2,8 @@ package sf.com.spb.domain;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
@@ -14,8 +16,17 @@ public class Persona implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPersona;
+    
+    @NotEmpty
     private String nombre;
+    
+    @NotEmpty
     private String apellido;
+    
+    @NotEmpty
+    @Email
     private String email;
+    
+    
     private String telefono;
 }
--- HolaMundoThymeleaf/src/main/java/sf/com/spb/web/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/web/ControladorInicio.java
@@ -1,9 +1,11 @@
 package sf.com.spb.web;
 
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sf.com.spb.domain.Persona;
@@ -30,8 +32,24 @@ public class ControladorInicio {
     }
     
     @PostMapping("/guardar")
-    public String guardar(Persona persona){
+    public String guardar(@Valid Persona persona, Errors errores){
+        if(errores.hasErrors()){
+            return "modificar";
+        }
         personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
         return "redirect:/";
     }
     
