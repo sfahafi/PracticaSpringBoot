5fd424241747726b0cac37bd6ee4607841eaf076
Administrador <Administrador@PC>
2020-08-05 10:54:14 -0300

    Crud con spring agregar

--- HolaMundoThymeleaf/src/main/java/sf/com/spb/web/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/web/ControladorInicio.java
@@ -1,25 +1,38 @@
 package sf.com.spb.web;
 
 import lombok.extern.slf4j.Slf4j;
-import sf.com.spb.dao.PersonaDao;
-import sf.com.spb.domain.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sf.com.spb.domain.Persona;
+import sf.com.spb.servicio.PersonaService;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
     
     @Autowired
-    private PersonaDao personaDao;
+    private PersonaService personaService;
     
     @GetMapping("/")
     public String inicio(Model model){
-        var personas = personaDao.findAll();
+        var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("personas", personas);
         return "index";
     }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
 }
