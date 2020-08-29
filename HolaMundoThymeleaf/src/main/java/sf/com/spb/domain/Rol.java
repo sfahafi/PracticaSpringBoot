1333e728c6d495ff8f1c8c4ec6a759cf3c521a93
fahaf <fahaf@192.168.0.10>
2020-08-29 13:53:36 -0300

    Seguridad con Spring y hibernate/JPA Finalizado

--- HolaMundoThymeleaf/src/main/java/sf/com/spb/domain/Rol.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/domain/Rol.java
@@ -5,13 +5,13 @@ import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Entity
-@Data
+@Data // proyecto de lombok para que genere los metodos get y set, entre otros
 @Table(name="rol")
 public class Rol implements Serializable{
     private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // forma de generacion de clave primaria
     private Long idRol;
     
     @NotEmpty
