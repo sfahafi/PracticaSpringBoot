95f908c99722f4f26f1c9f75cb4e07b485849a5a
fahaf <fahaf@192.168.0.10>
2020-09-26 21:41:15 -0300

    Proyecto terminado - Mejorado

--- HolaMundoThymeleaf/src/main/java/sf/com/spb/util/EncriptarPassword.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/util/EncriptarPassword.java
@@ -4,8 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class EncriptarPassword {
     public static void main(String[] args) {        
-        var password = "123";
-        System.out.println("Password" + password);
+        var password = "springboot";
+        System.out.println("Password " + password);
         System.out.println("Password encriptado: " + encriptarPassword(password));
     }
     
--- HolaMundoThymeleaf/src/main/java/sf/com/spb/web/SecurityConfig.java
+++ HolaMundoThymeleaf/src/main/java/sf/com/spb/web/SecurityConfig.java
@@ -44,9 +44,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/editar/**","/agregar/**","/eliminar")
-                    .hasRole("ADMIN")
-                .antMatchers("/")
+                .antMatchers("/editar/**","/eliminar")
+                    .hasRole("ADMIN")                
+                .antMatchers("/agregar/**","/")
                     .hasAnyRole("USER","ADMIN")
                 .and()
                     .formLogin()
